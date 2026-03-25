adeb424dbdfd56d4a5f62ef9ff7276cb9ef4fa93
Dungnn <nguyenngocdung19122005@gmail.com>
2026-03-25 11:02:23 +0700

    Upgrade fading system.

--- Assets/Scripts/FadeOnBlock.cs
+++ Assets/Scripts/FadeOnBlock.cs
@@ -1,49 +1,77 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FadeOnBlock : MonoBehaviour
 {
     public Transform target;
     public LayerMask wallLayer;
-    public float fadeAlpha = 0.3f;
+    public Material fadeMaterial;
 
-    private Renderer lastRenderer;
-    private Color originalColor;
+    [Range(0.01f, 20f)] public float fadeSpeed = 8f;
+
+    // lưu renderer + material gốc
+    private Dictionary<Renderer, Material[]> originalMats = new Dictionary<Renderer, Material[]>();
+    private HashSet<Renderer> currentFaded = new HashSet<Renderer>();
 
     void Update()
     {
-        Ray ray = new Ray(transform.position, target.position - transform.position);
-        RaycastHit hit;
+        Vector3 camPos = transform.position;
+        Vector3 targetPos = target.position;
 
-        if (Physics.Raycast(ray, out hit, 100f, wallLayer))
+        Vector3 dir = (targetPos - camPos).normalized;
+        float distance = Vector3.Distance(camPos, targetPos);
+
+        RaycastHit[] hits = Physics.RaycastAll(camPos, dir, distance, wallLayer);
+
+        HashSet<Renderer> newFaded = new HashSet<Renderer>();
+
+        foreach (var hit in hits)
         {
-            Renderer r = hit.collider.GetComponent<Renderer>();
+            Renderer[] renderers = hit.collider.GetComponentsInChildren<Renderer>();
 
-            if (r != null)
+            foreach (var r in renderers)
             {
-                if (lastRenderer != r)
-                {
-                    ResetLast();
-                    lastRenderer = r;
-                    originalColor = r.material.color;
-                }
+                newFaded.Add(r);
 
-                Color c = r.material.color;
-                c.a = fadeAlpha;
-                r.material.color = c;
+                // nếu chưa từng fade
+                if (!originalMats.ContainsKey(r))
+                {
+                    originalMats[r] = r.materials;
+                    ApplyFadeMaterial(r);
+                }
             }
         }
-        else
+
+        // reset những object không còn bị che
+        foreach (var r in currentFaded)
         {
-            ResetLast();
+            if (!newFaded.Contains(r))
+            {
+                RestoreMaterial(r);
+            }
         }
+
+        currentFaded = newFaded;
     }
 
-    void ResetLast()
+    void ApplyFadeMaterial(Renderer r)
     {
-        if (lastRenderer != null)
+        Material[] mats = new Material[r.materials.Length];
+
+        for (int i = 0; i < mats.Length; i++)
         {
-            lastRenderer.material.color = originalColor;
-            lastRenderer = null;
+            mats[i] = fadeMaterial;
+        }
+
+        r.materials = mats;
+    }
+
+    void RestoreMaterial(Renderer r)
+    {
+        if (originalMats.ContainsKey(r))
+        {
+            r.materials = originalMats[r];
+            originalMats.Remove(r);
         }
     }
 }
